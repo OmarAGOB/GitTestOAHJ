3cc0e1d26adc268b7ae318b5faa7dd0037f31468
Omar <OAgob@bhakwien10.at>
2024-02-29 10:45:00 +0100

    Add project files.

---
+++ GitTestOAHJ/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace GitTestOAHJ
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
